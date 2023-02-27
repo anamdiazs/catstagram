cbea8dd6f6d2a15951ee4e0f312d77ecf5a28c8a
Ana María Díaz <anamdiazs1212@gmail.com>
2023-02-26 23:25:17 -0600

    feat: Create  Image Modal

---
+++ app/components/Modal.jsx
@@ -0,0 +1,17 @@
+
+'use client'
+
+export default function Modal() {
+
+  return (
+	 <div>
+		<div className="w-96 h-96 bg-white rounded-sm shadow-sm">
+			<h1>Persian Cat</h1>
+			<img className="w-full h-full object-cover" src="" alt="" />
+			<p>
+				This breed is expensive
+			</p>
+		</div>
+	 </div>
+  )
+}
