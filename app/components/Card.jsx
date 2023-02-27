04e5b5a03bd4ed9111d118eae5563855b1750415
Ana María Díaz <anamdiazs1212@gmail.com>
2023-02-27 12:41:22 -0600

    feat: Implement intersection observer

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -1,9 +1,17 @@
+'use client'
+
+import useIntersectionObserver from "../hooks/useIntersectionObserver"
+
+import { useRef } from "react";
 
 export default function Card(props) {
 	const { image } = props
+	const cardRef = useRef(null)
+	const isIntersecting = useIntersectionObserver(cardRef)
   return (
-	 <div className='w-32 h-32 lg:w-96 lg:h-96'>
+	 <div className='w-32 h-32 lg:w-96 lg:h-96' ref={cardRef}>
 		<img className=" w-full h-full object-cover" src={image} alt='cat-image'/>
+		{isIntersecting ? "Itersecting" : "Not intersecting"}
 	 </div>
   )
 }
