5e4bfe22fa47c88c33d6bdb648d18e8df11ce439
Ana María Díaz <anamdiazs1212@gmail.com>
2023-02-26 22:01:36 -0600

    feat: Create custom Intersection Observer hook

---
+++ app/hooks/useIntersectionObserver.jsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { useState, useEffect, useRef } from "react";
+
+const useIntersectionObserver = (ref, options = {} ) => {
+
+	const [isIntersecting, setIsIntersecting] = useState(false);
+	const observerRef = useRef(null);
+	useEffect(() => {
+		if (!ref.current) return;
+
+		const handleIntersect = (entries) => {
+			setIsIntersecting(entries[0].isIntersecting);
+		};
+
+		observerRef.current = new IntersectionObserver(handleIntersect, options);
+		observerRef.current.observe(ref.current);
+
+		return () => {
+			observerRef.current.disconnect();
+		};
+	}, [ref, options]);
+
+	return isIntersecting;
+}
+export default useIntersectionObserver;
