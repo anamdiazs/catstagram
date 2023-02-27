8cf1579c2f1b2dd27ec41bb086953d0db45fac1e
Ana María Díaz <anamdiazs1212@gmail.com>
2023-02-27 16:34:15 -0600

    feat: Update navbar color

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar(props) {
 	 <nav className="w-full h-16 bg-rose-200 p-4 flex items-center z-50 justify-between">
 		<div className="flex space-x-4 items-center lg:space-x-10">
 			<img className="w-10 h-10" src="https://cdn-icons-png.flaticon.com/512/2865/2865523.png"/>
-			<h1 className="text-white text-lg lg:text-xl">Catstagram</h1>
+			<h1 className="text-gray-500 text-lg lg:text-xl">Catstagram</h1>
 		</div>
 		<Dropdown breeds={breeds} />
 	 </nav>
